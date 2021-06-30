9793d7296a5fefa978149963ce8206296ee07cdc
Nick <nick.fedorov.root@gmail.com>
2021-06-30 04:00:54 +0300

    clipboard

--- src/components/CutLinkBanner.vue
+++ src/components/CutLinkBanner.vue
@@ -13,30 +13,34 @@
                 <div class="flex lg:items-center lg:flex-row flex-col pt-2 pb-[18px] border-b border-black-10 border-1 md:pr-[24px]"><span class="flex flex-grow text-black-60">https://www.youtube.com/watch?v=dQw4w9WgXcQ</span>
                   <div class="flex lg:mt-0 mt-2 lg:items-center"><span class="badge--green md:mr-[45px] mr-auto inline-flex font-medium border-2 rounded-[4px] px-[18.5px] py-[6.5px]  items-center">qib.li/sdfh3 </span>
                     <button type="button" class="mr-[16px] flex items-center">
-                      <svg-icon name='copy'/>
+                      <SvgIcon name='copy'/>
                     </button>
                     <button type="button"  class="flex items-center" @click="isModalQrVisible = true">
-                      <svg-icon name='qr'/>
+                      <SvgIcon name='qr'/>
                     </button>
                   </div>
                 </div>
                 <div class="flex lg:items-center lg:flex-row flex-col pt-2 pb-[18px] border-b border-black-10 border-1 md:pr-[24px]"><span class="flex flex-grow text-black-60">https://www.youtube.com/watch?v=dQw4w9WgXcQ</span>
-                  <div class="flex lg:mt-0 mt-2 lg:items-center"><span class="badge--green md:mr-[45px] mr-auto inline-flex font-medium border-2 rounded-[4px] px-[18.5px] py-[6.5px]  items-center">qib.li/sdfh3 </span>
-                    <button type="button" class="mr-[16px] flex items-center">
-                      <svg-icon name='copy'/>
+                  <div class="flex lg:mt-0 mt-2 lg:items-center">
+                    <span class="badge--green md:mr-[45px] mr-auto inline-flex font-medium border-2 rounded-[4px] px-[18.5px] py-[6.5px]  items-center">qib.li/sdfh3 </span>
+                    <input type="text" class='testing-code' value="11111">
+                    <button type="button" class="mr-[16px] flex items-center"  @click.stop.prevent="copyTestingCode">
+                      <SvgIcon name='copy'/>
                     </button>
                     <button type="button"  class="flex items-center" @click="isModalQrVisible = true">
-                      <svg-icon name='qr'/>
+                      <SvgIcon name='qr'/>
                     </button>
                   </div>
                 </div>
                 <div class="flex lg:items-center lg:flex-row flex-col pt-2 pb-[18px]  md:pr-[24px] "><span class="flex flex-grow text-black-60">https://www.youtube.com/watch?v=dQw4w9WgXcQ</span>
-                  <div class="flex lg:mt-0 mt-2 lg:items-center"><span class="badge--green md:mr-[45px] mr-auto inline-flex font-medium border-2 rounded-[4px] px-[18.5px] py-[6.5px]  items-center">qib.li/sdfh3 </span>
-                    <button type="button" class="mr-[16px] flex items-center">
-                      <svg-icon name='copy'/>
+                  <div class="flex lg:mt-0 mt-2 lg:items-center">
+                    <span class="badge--green md:mr-[45px] mr-auto inline-flex font-medium border-2 rounded-[4px] px-[18.5px] py-[6.5px]  items-center" >qib.li/sdfh3 </span>
+                    <input type="text" class='testing-code' value="564654">
+                    <button type="button" class="mr-[16px] flex items-center" @click.stop.prevent="copyTestingCode">
+                      <SvgIcon name='copy'/>
                     </button>
                     <button type="button"  class="flex items-center" @click="isModalQrVisible = true">
-                      <svg-icon name='qr'/>
+                      <SvgIcon name='qr'/>
                     </button>
                   </div>
                 </div>
@@ -62,6 +66,26 @@ export default {
       return {
         isModalQrVisible: false,
       };
-    }
+    },
+  methods: {
+        copyTestingCode () {
+          let testingCodeToCopy = document.querySelector('.testing-code')
+          testingCodeToCopy.setAttribute('type', 'text')    // 不是 hidden 才能複製
+          testingCodeToCopy.select()
+
+          try {
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'successful' : 'unsuccessful';
+            alert('Testing code was copied ' + msg);
+          } catch (err) {
+            alert('Oops, unable to copy');
+          }
+
+          /* unselect the range */
+          testingCodeToCopy.setAttribute('type', 'hidden')
+          window.getSelection().removeAllRanges()
+        },
+    },
 }
+console.log(document.querySelectorAll('.testing-code'))
 </script>
--- vue.config.js
+++ vue.config.js
@@ -7,27 +7,21 @@ module.exports = {
   },
   pluginOptions: {
     svgSprite: {
-        /*
-         * The directory containing your SVG files.
-         */
-        dir: 'src/assets/icons',
-        /*
-         * The reqex that will be used for the Webpack rule.
-         */
-        test: /\.(svg)(\?.*)?$/,
-        /*
-         * @see https://github.com/kisenka/svg-sprite-loader#configuration
-         */
-        loaderOptions: {
-            extract: true,
-            spriteFilename: 'img/icons.[hash:8].svg' // or 'img/icons.svg' if filenameHashing == false
-        },
-        /*
-         * @see https://github.com/kisenka/svg-sprite-loader#configuration
-         */
-        pluginOptions: {
-            plainSprite: true
-        }    
-    }    
-}
+      dir: 'src/assets/icons',
+      test: /\.(svg)(\?.*)?$/,
+      loaderOptions: {
+        extract: true,
+        spriteFilename: 'img/icons.[hash:8].svg' // or 'img/icons.svg' if filenameHashing == false
+      },
+      pluginOptions: {
+        plainSprite: true
+      }
+    }
+  },
+  devServer: {
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  }
 }
